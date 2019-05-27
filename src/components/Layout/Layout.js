fcca1ff45e795a36a978609d944b82f2a5259069
Viktor Orlyk <victor.orlyk93@gmail.com>
2019-05-27 13:42:05 +0300

    for ejection

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import Aux from '../../hoc/Aux';
+import styles from './Layout.css'
+
+const Layout = (props) => {
+  return (
+    <Aux>
+      <div>
+        Toolbar, SideDrawer, Backdrop
+      </div>
+      <main className={styles.Content}>{props.children}</main>
+    </Aux>
+
+  );
+};
+
+export default Layout;
+
---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './App.css';
+import Layout from '../components/Layout/Layout';
+import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Layout>
+        <BurgerBuilder/>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
